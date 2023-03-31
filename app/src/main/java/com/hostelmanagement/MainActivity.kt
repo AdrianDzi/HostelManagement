43803a0261a3f0550c7810807816f08163a73b99
AdrianDzi <adrian.dziobiak@gmail.com>
2023-03-31 16:51:05 +0200

    createn bottom nav bar, with empty fragments on main screen

--- app/src/main/java/com/hostelmanagement/MainActivity.kt
+++ app/src/main/java/com/hostelmanagement/MainActivity.kt
@@ -3,9 +3,13 @@ package com.hostelmanagement
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.hostelmanagement.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.hostelmanagement.accountService.LoginActivity
+import com.hostelmanagement.home.HomeFragment
+import com.hostelmanagement.hostel.HostelFragment
+import com.hostelmanagement.renting.RentingFragment
+import com.hostelmanagement.user.UserFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,22 +18,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        replaceFragment(HostelFragment())
         firebaseAuth = FirebaseAuth.getInstance()
-        if (firebaseAuth.currentUser != null) {
-            firebaseAuth.currentUser?.let {
-                binding.tvUserEmailMainActivity.text = it.email
+
+        binding.bnNavMainActivity.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.hostel -> replaceFragment(HostelFragment())
+                R.id.user -> replaceFragment(UserFragment())
+                R.id.renting -> replaceFragment(RentingFragment())
+                else ->{
+
+                }
             }
+            true
         }
-        binding.btnLogoutMainActivity.setOnClickListener {
-            firebaseAuth.signOut()
-            startActivity(
-                Intent(
-                    this,
-                    LoginActivity::class.java
-                )
-            )
-            finish()
-        }
+
+//        if (firebaseAuth.currentUser != null) {
+//            firebaseAuth.currentUser?.let {
+//                binding.tvUserEmailMainActivity.text = it.email
+//            }
+//        }
+//        binding.btnLogoutMainActivity.setOnClickListener {
+//            firebaseAuth.signOut()
+//            startActivity(
+//                Intent(
+//                    this,
+//                    LoginActivity::class.java
+//                )
+//            )
+//            finish()
+//        }
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fl_frameMainActivity, fragment)
+        fragmentTransaction.commit()
     }
 }
